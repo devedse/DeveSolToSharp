c309f5a323f556d75aa675efcdb6cadbde1840ff
Devedse <davydavidse@gmail.com>
2018-12-20 15:44:59 +0100

    Added 1 test

---
+++ DeveSolToSharp.Tests/Helpers/StringHelperFacts.cs
@@ -0,0 +1,21 @@
+﻿using DeveSolToSharp.Helpers;
+using Xunit;
+
+namespace DeveSolToSharp.Tests.Helpers
+{
+    public class StringHelperFacts
+    {
+        [Fact]
+        public void FirstCharToUpperReturnsFirstCharToUpper()
+        {
+            //Arrange
+            var input = "test";
+
+            //Act
+            var result = StringHelper.FirstCharToUpper(input);
+
+            //Assert
+            Assert.Equal("Test", result);
+        }
+    }
+}
