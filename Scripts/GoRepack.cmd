5f1dd94edda640801cb3c1d8c34dff68abeb0041
Devedse <davydavidse@gmail.com>
2018-12-20 15:40:01 +0100

    Fix appveyor build

--- Scripts/GoRepack.cmd
+++ Scripts/GoRepack.cmd
@@ -1,6 +1,6 @@
 call "%~dp0Cleanup.cmd"
 
-robocopy "%~dp0..\DeveSolToSharp\bin\Release\netstandard2.1" "%~dp0Output" /E /xf *.pdb
+robocopy "%~dp0..\DeveSolToSharp\bin\Release\netcoreapp2.2" "%~dp0Output" /E /xf *.pdb
 
 "%~dp0\7z_x64_1805\7za.exe" a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveSolToSharp.zip" "%~dp0Output\*"
 "%~dp0\7z_x64_1805\7za.exe" a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveSolToSharp.7z" "%~dp0Output\*"
